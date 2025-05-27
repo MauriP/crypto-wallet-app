5c5ebe10c75507083d3c27f74ca2a1a54efd645f
Mauri Perret <mauriperret5@gmail.com>
2025-05-27 09:42:14 -0300

    Primer commit: estructura inicial de backend, frontend vacio

---
+++ BackEnd/ApiWallet/Models/Entyties/CryptoPrice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiWallet.Models.Entyties;
+
+public partial class CryptoPrice
+{
+    public int Id { get; set; }
+
+    public int CryptoId { get; set; }
+
+    public int ExchangeId { get; set; }
+
+    public decimal? BuyPrice { get; set; }
+
+    public decimal? SellPrice { get; set; }
+
+    public DateTime? LastUpdated { get; set; }
+
+    public virtual Cryptocurrency Crypto { get; set; } = null!;
+
+    public virtual Exchange Exchange { get; set; } = null!;
+}
---
+++ BackEnd/ApiWallet/Models/Entyties/VWalletSummary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiWallet.Models.Entyties;
+
+public partial class VWalletSummary
+{
+    public int UserId { get; set; }
+
+    public string CryptoCode { get; set; } = null!;
+
+    public string CryptoName { get; set; } = null!;
+
+    public decimal? TotalAmount { get; set; }
+
+    public decimal? CurrentPrice { get; set; }
+
+    public decimal? CurrentValue { get; set; }
+}
