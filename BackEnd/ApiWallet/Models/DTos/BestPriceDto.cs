e7ac97eebc0750f7801e7a324f98246b506dde16
Mauricio Perret <mauriperret5@gmail.com>
2025-06-02 14:07:20 -0300

    Delete BackEnd/ApiWallet directory
    elimino este directorio porque ya esta dentro de src

--- BackEnd/ApiWallet/Models/DTos/BestPriceDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApiWallet.Models.DTos
-{
-    public class BestPriceDto
-    {
-        public string ExchangeCode { get; set; } = null!;
-        public decimal Price { get; set; }
-        public DateTime LastUpdate { get; set; }
-    }
-}
